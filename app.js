52b80d3c85de48e17dbd4dc7199ce6a562d8e331
maharjansuren37 <maharjansuren37@gmail.com>
2025-02-19 21:32:26 +1030

    add feature: edit and toast

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const addBtn = document.querySelector('.add');
 const output = document.getElementById('output');
-addBtn.addEventListener('click', addNote);
+const content = document.querySelector('.content');
 
-function addNote(e) {
-    let note = input.value;
+// Event Listeners
+addBtn.addEventListener('click', addTask);
+
+function addTask(e) {
+    let task = input.value;
 
     const li = document.createElement('li');
-    const text = document.createTextNode(note);
-    output.appendChild(text);
+    li.appendChild(document.createTextNode(task));
+    li.className = 'task';
+    const edit = document.createElement('a');
+    edit.innerHTML = 'Edit';
+    edit.className = 'edit';
+    li.appendChild(edit);
+    output.appendChild(li);
+
+    displayMessage("Task added!", "normal");
 
     input.value = "";
 
     e.preventDefault();
+}
+
+function displayMessage(message, classname) {
+    const msg = document.createElement('div');
+    msg.appendChild(document.createTextNode(message));
+    msg.className = `${classname}`;
+    content.insertBefore(msg, form);
+
+    setTimeout(() => {
+        msg.remove('div');
+    }, 3000);
 }
