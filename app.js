5e78f27ed8f4b65842ee8876e703177f78321c54
maharjansuren37 <maharjansuren37@gmail.com>
2025-02-23 18:03:28 +1030

    feat: delete task

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ const input = document.getElementById('input');
 const addBtn = document.querySelector('.add');
 const output = document.getElementById('output');
 const content = document.querySelector('.content');
+const heading = document.querySelector('.heading');
 
 // Event Listeners
 addBtn.addEventListener('click', addTask);
+output.addEventListener('click', deleteTask);
 
 function addTask(e) {
     let task = input.value;
 
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(task));
-    li.className = 'task';
-    const edit = document.createElement('a');
-    edit.innerHTML = 'Edit';
-    edit.className = 'edit';
-    li.appendChild(edit);
-    output.appendChild(li);
+    if (task !== '') {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(task));
+        li.className = 'task';
+        const edit = document.createElement('a');
+        edit.innerHTML = '<span>Delete</span>';
+        edit.className = 'edit';
+        li.appendChild(edit);
+        output.appendChild(li);
 
-    displayMessage("Task added!", "normal");
+        displayMessage("Task added!", "normal");
 
-    input.value = "";
+        input.value = "";
+    } else {
+        displayMessage("Please add a task!", 'alert');
+    }
+
+    
 
     e.preventDefault();
 }
@@ -29,10 +37,17 @@ function addTask(e) {
 function displayMessage(message, classname) {
     const msg = document.createElement('div');
     msg.appendChild(document.createTextNode(message));
-    msg.className = `${classname}`;
-    content.insertBefore(msg, form);
+    msg.className = `message ${classname}`;
+    content.insertBefore(msg, heading);
 
     setTimeout(() => {
         msg.remove('div');
     }, 3000);
+}
+
+function deleteTask(e) {
+    if (e.target.innerText === 'Delete') {
+        confirm('Are you sure?')
+        e.target.parentElement.parentElement.remove();
+    }
 }
